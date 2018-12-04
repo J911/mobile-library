7c687d5dbff8abc8ffcb7026ffa4c1341c8cf18e
Jay Lee <jay@j911.me>
2018-12-04 09:03:29 +0900

    feat: add Slpash Activity: Splash.java

---
+++ app/src/main/java/me/j911/term_project/mobile_library/Splash.java
@@ -0,0 +1,26 @@
+package me.j911.term_project.mobile_library;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
