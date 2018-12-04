c9de5c464c917ca54e4ad605f57f7ba875c8e733
Jay Lee <jay@j911.me>
2018-12-04 11:24:07 +0900

    feat: impliment signin, signup
    ned

--- app/src/main/java/me/j911/term_project/mobile_library/MainActivity.java
+++ app/src/main/java/me/j911/term_project/mobile_library/MainActivity.java
@@ -2,9 +2,6 @@ package me.j911.term_project.mobile_library;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import me.j911.term_project.mobile_library.controller.AccountController;
 
 public class MainActivity extends AppCompatActivity {
 
