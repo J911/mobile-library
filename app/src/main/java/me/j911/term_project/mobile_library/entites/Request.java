4334fcfe3da4f43a278f6e5e016f1d50c7730e5f
Jay Lee <jay@j911.me>
2018-12-06 21:34:08 +0900

    feat: add Request properties

--- app/src/main/java/me/j911/term_project/mobile_library/entites/Request.java
+++ app/src/main/java/me/j911/term_project/mobile_library/entites/Request.java
@@ -4,14 +4,36 @@ import me.j911.term_project.mobile_library.interfaces.IRequest;
 
 public class Request implements IRequest {
 
-    @Override
-    public int getStdLike() {
-        return 0;
+    private String title;
+    private String contents;
+    private int like;
+    private int stdId;
+
+    public Request(String title, String contents, int stdId, int like) {
+        this.title = title;
+        this.contents = contents;
+        this.stdId = stdId;
+        this.like = like;
     }
 
     @Override
-    public int getReservedStdId() {
-        return 0;
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String getContents() {
+        return contents;
+    }
+
+    @Override
+    public int getStdLike() {
+        return like;
+    }
+
+    @Override
+    public int getRequestStdId() {
+        return stdId;
     }
 
 }
--- app/src/main/java/me/j911/term_project/mobile_library/interfaces/IRequest.java
+++ app/src/main/java/me/j911/term_project/mobile_library/interfaces/IRequest.java
@@ -1,6 +1,8 @@
 package me.j911.term_project.mobile_library.interfaces;
 
 public interface IRequest {
-    public int getReservedStdId();
+    public String getTitle();
+    public String getContents();
+    public int getRequestStdId();
     public int getStdLike();
 }
